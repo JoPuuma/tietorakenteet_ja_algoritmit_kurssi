020e4cab417fdcf14da350f07290ad858cf097fb
Joonas Puumala <joonas.puumala@tuni.fi>
2020-04-29 21:14:34 +0300

    prg1 merged

--- prg2/datastructures.cc
+++ prg2/datastructures.cc
@@ -19,10 +19,6 @@ Type random_in_range(Type start, Type end)
     return static_cast<Type>(start+num);
 }
 
-// Modify the code below to implement the functionality of the class.
-// Also remove comments from the parameter names when you implement
-// an operation (Commenting out parameter name prevents compiler from
-// warning about unused parameters on operations you haven't yet implemented.)
 
 Datastructures::Datastructures()
 {
@@ -36,146 +32,337 @@ Datastructures::~Datastructures()
 
 int Datastructures::stop_count()
 {
-    // Replace this comment and the line below with your implementation
-    return NO_VALUE;
+    return stopsByID.size();
 }
 
 void Datastructures::clear_all()
 {
-    // Replace this comment with your implementation
+    stopsByID.clear();
+    regionsByID.clear();
+    stopCoords.clear();
+    stopsByName.clear();
 }
 
 std::vector<StopID> Datastructures::all_stops()
 {
-    // Replace this comment and the line below with your implementation
-    return {NO_STOP};
+    std::vector<StopID> stops = {};
+    for(auto& stop : stopsByID)
+    {
+        stops.push_back(stop.first);
+    }
+    return stops;
 }
 
 bool Datastructures::add_stop(StopID id, const Name& name, Coord xy)
 {
-    // Replace this comment and the line below with your implementation
-    return false;
+    if(stopsByID.find(id) != stopsByID.end())
+    {
+        return false;
+    }
+    else
+    {
+        std::shared_ptr<Stop> newStop = std::make_shared<Stop>(Stop{id, xy, name});
+        stopsByID[id] = newStop;
+
+        Stop* stopPtr = &(*newStop);
+        stopCoords.insert(std::pair<Coord, Stop*>(xy,stopPtr));
+        stopsByName.insert(std::pair<std::string,Stop*>(name,stopPtr));
+        return true;
+    }
 }
 
 Name Datastructures::get_stop_name(StopID id)
 {
-    // Replace this comment and the line below with your implementation
-    return NO_NAME;
+    auto it = stopsByID.find(id);
+    if(it == stopsByID.end()) return NO_NAME;
+    else return it->second->name_;
 }
 
 Coord Datastructures::get_stop_coord(StopID id)
 {
-    // Replace this comment and the line below with your implementation
-    return NO_COORD;
+    auto it = stopsByID.find(id);
+    if(it == stopsByID.end()) return NO_COORD;
+    else return it->second->coord_;
 }
 
 std::vector<StopID> Datastructures::stops_alphabetically()
 {
-    // Replace this comment and the line below with your implementation
-    return {NO_STOP};
+    std::vector<StopID> ordered = {};
+    for(auto stop : stopsByName)
+    {
+        ordered.push_back(stop.second->id_);
+    }
+
+    return ordered;
 }
+
 
 std::vector<StopID> Datastructures::stops_coord_order()
 {
-    // Replace this comment and the line below with your implementation
-    return {NO_STOP};
+    std::vector<StopID> ordered = {};
+    for(auto stop : stopCoords)
+    {
+        ordered.push_back(stop.second->id_);
+    }
+
+    return ordered;
 }
 
 StopID Datastructures::min_coord()
 {
-    // Replace this comment and the line below with your implementation
-    return NO_STOP;
+    if(stop_count() == 0) return NO_STOP;
+    return stopCoords.begin()->second->id_;
 }
 
 StopID Datastructures::max_coord()
 {
-    // Replace this comment and the line below with your implementation
-    return NO_STOP;
+    if(stop_count() == 0) return NO_STOP;
+    return stopCoords.rbegin()->second->id_;
 }
 
 std::vector<StopID> Datastructures::find_stops(Name const& name)
 {
-    // Replace this comment and the line below with your implementation
-    return {NO_STOP};
+    std::vector<StopID> stops = {};
+    std::pair <std::multimap<std::string,Stop*>::iterator, std::multimap<std::string,Stop*>::iterator> ret;
+    ret = stopsByName.equal_range(name);
+    for (std::multimap<std::string,Stop*>::iterator it=ret.first; it!=ret.second; ++it)
+    {
+      stops.push_back(it->second->id_);
+    }
+    return stops;
 }
 
 bool Datastructures::change_stop_name(StopID id, const Name& newname)
 {
-    // Replace this comment and the line below with your implementation
-    return false;
+    auto stopIt = stopsByID.find(id);
+    if(stopIt == stopsByID.end()) return false;
+    // stopsByName
+    Stop* stopPtr = &(*stopIt->second);
+    std::pair <std::multimap<std::string,Stop*>::iterator, std::multimap<std::string,Stop*>::iterator> nameRet;
+    nameRet = stopsByName.equal_range(stopIt->second->name_);
+    for (std::multimap<std::string,Stop*>::iterator it=nameRet.first; it!=nameRet.second; ++it)
+    {
+        if(it->second->id_ == id)
+        {
+            stopsByName.erase(it);
+            stopsByName.insert(std::make_pair(newname, stopPtr));
+            break;
+        }
+    }
+    // stops object itself
+    stopIt->second->name_ = newname;
+    return true;
 }
 
 bool Datastructures::change_stop_coord(StopID id, Coord newcoord)
 {
-    // Replace this comment and the line below with your implementation
-    return false;
+    auto stopIt = stopsByID.find(id);
+
+    if(stopIt == stopsByID.end()) return false;
+    else
+    {
+        std::pair <std::multimap<Coord,Stop*>::iterator, std::multimap<Coord,Stop*>::iterator> ret;
+        Stop* stopPtr = &(*stopIt->second);
+        // update new coord to the stopCoords structure
+        ret = stopCoords.equal_range(stopPtr->coord_);
+        for (std::multimap<Coord,Stop*>::iterator it=ret.first; it!=ret.second; ++it)
+        {
+            if(it->second->id_ == id)
+            {
+                stopCoords.erase(it);
+                stopCoords.insert(std::make_pair(newcoord,stopPtr));
+                break;
+            }
+        }
+        // update new coord to the regoin of the stop
+        if(stopIt->second->region_ != nullptr)
+        {
+            std::multimap<Coord, Stop*>* m = &(stopIt->second->region_->stops_);
+            ret = m->equal_range(stopIt->second->coord_);
+            for (std::multimap<Coord,Stop*>::iterator it=ret.first; it!=ret.second; ++it)
+            {
+                if(it->second->id_ == id)
+                {
+                    Region* regionPtr = &(*stopPtr->region_);
+                    regionPtr->stops_.erase(it);
+                    regionPtr->stops_.insert(std::make_pair(newcoord,stopPtr));
+                    regionPtr->minMaxIsValid = false;
+                    break;
+                }
+            }
+        }
+        // update stopsByID (stop itself)
+        stopPtr->coord_ = newcoord;
+
+        return true;
+    }
 }
 
 bool Datastructures::add_region(RegionID id, const Name &name)
 {
-    // Replace this comment and the line below with your implementation
-    return false;
+    if(regionsByID.find(id) == regionsByID.end())
+    {
+        Region newRegion = {id, name, nullptr, {},{},{},false};
+        regionsByID[id] = newRegion;
+        return true;
+    }
+    else return false;
 }
 
 Name Datastructures::get_region_name(RegionID id)
 {
-    // Replace this comment and the line below with your implementation
-    return NO_NAME;
+    auto it = regionsByID.find(id);
+    if(it == regionsByID.end()) return NO_NAME;
+    else return it->second.name_;
 }
 
 std::vector<RegionID> Datastructures::all_regions()
 {
-    // Replace this comment and the line below with your implementation
-    return {NO_REGION};
+    std::vector<RegionID> regions = {};
+    for(auto& region : regionsByID)
+    {
+        regions.push_back(region.first);
+    }
+    return regions;
 }
 
 bool Datastructures::add_stop_to_region(StopID id, RegionID parentid)
 {
-    // Replace this comment and the line below with your implementation
-    return false;
+    auto stopIt = stopsByID.find(id);
+    auto regionIt = regionsByID.find(parentid);
+
+    if(stopIt == stopsByID.end() || regionIt == regionsByID.end()) return false;
+    Stop* stopPtr = &(*stopIt->second);
+    if(stopPtr->region_ != nullptr) return false;
+    // region to stop
+    stopPtr->region_ = &regionIt->second;
+    // stop to region
+    regionIt->second.stops_.insert(std::make_pair(stopPtr->coord_,stopPtr));
+    regionIt->second.minMaxIsValid = false;
+    return true;
 }
 
 bool Datastructures::add_subregion_to_region(RegionID id, RegionID parentid)
 {
-    // Replace this comment and the line below with your implementation
-    return false;
+    auto subIt = regionsByID.find(id);
+    auto parentIt = regionsByID.find(parentid);
+
+    if(subIt == regionsByID.end() || parentIt == regionsByID.end()) return false;
+
+    Region* subRegion = &subIt->second;
+    if(subRegion->overRegion_ != nullptr) return false; // has already overregion
+
+    Region* parentRegion = &parentIt->second;
+    Region* parentOverRegion = parentRegion->overRegion_;
+    if(parentOverRegion != nullptr)
+    {
+        if( parentOverRegion->id_ == subRegion->id_) return false; // cannot make loop between regions
+    }
+
+    // add regions to datastructure elements
+    subRegion->overRegion_ = parentRegion;
+    parentRegion->subRegions_.push_back(subRegion);
+    parentRegion->minMaxIsValid = false;
+    return true;
 }
 
 std::vector<RegionID> Datastructures::stop_regions(StopID id)
 {
-    // Replace this comment and the line below with your implementation
-    return {NO_REGION};
+    std::vector<RegionID> regions = {};
+    auto stop = stopsByID.find(id);
+
+    if(stop == stopsByID.end())
+    {
+        regions.push_back(NO_REGION);
+        return regions;
+    }
+    getRegions(regions, stop->second->region_);
+    return regions;
 }
 
 void Datastructures::creation_finished()
 {
-    // Replace this comment with your implementation
-    // You don't have to use this method for anything, if you don't need it
+
 }
 
 std::pair<Coord,Coord> Datastructures::region_bounding_box(RegionID id)
 {
-    // Replace this comment and the line below with your implementation
-    return {NO_COORD, NO_COORD};
+    std::pair<Coord,Coord> minMax = {NO_COORD, NO_COORD};
+    auto regionIt = regionsByID.find(id);
+    if(regionIt == regionsByID.end()) return minMax;
+    Region* regionPtr = &(regionIt->second);
+    getMinMaxCoords(minMax.first, minMax.second, regionPtr);
+    return minMax;
 }
 
 std::vector<StopID> Datastructures::stops_closest_to(StopID id)
 {
+    (void)id;
     // Replace this comment and the line below with your implementation
     return {NO_STOP};
 }
 
 bool Datastructures::remove_stop(StopID id)
 {
-    // Replace this comment and the line below with your implementation
-    return false;
+    auto stopIt = stopsByID.find(id);
+    if(stopIt == stopsByID.end()) return false;
+    Stop* stopPtr = &(*stopIt->second);
+    Coord* coord = &stopPtr->coord_;
+    std::pair <std::multimap<Coord,Stop*>::iterator, std::multimap<Coord,Stop*>::iterator> ret;
+    std::pair <std::multimap<std::string,Stop*>::iterator, std::multimap<std::string,Stop*>::iterator> nameRet;
+
+    // stopsByName
+    nameRet = stopsByName.equal_range(stopIt->second->name_);
+    for (std::multimap<std::string,Stop*>::iterator it=nameRet.first; it!=nameRet.second; ++it)
+    {
+        if(it->second->id_ == id)
+        {
+            stopsByName.erase(it);
+            break;
+        }
+    }
+    // stopCoords
+    ret = stopCoords.equal_range(*coord);
+    for (std::multimap<Coord,Stop*>::iterator it=ret.first; it!=ret.second; ++it)
+    {
+        if(it->second->id_ == id)
+        {
+            stopCoords.erase(it);
+            break;
+        }
+    }
+    // region of the stop
+    if(stopPtr->region_ != nullptr)
+    {
+        std::multimap<Coord, Stop*>* m = &(stopPtr->region_->stops_);
+        ret = m->equal_range(stopPtr->coord_);
+        for (std::multimap<Coord,Stop*>::iterator it=ret.first; it!=ret.second; ++it)
+        {
+            if(it->second->id_ == id)
+            {
+                Region* regionPtr = &(*stopPtr->region_);
+                regionPtr->stops_.erase(it);
+                break;
+            }
+        }
+        updateRegionMinMax(stopPtr->region_ );
+    }
+    // stopsByID
+    stopsByID.erase(stopIt);
+    return true;
 }
 
 RegionID Datastructures::stops_common_region(StopID id1, StopID id2)
 {
-    // Replace this comment and the line below with your implementation
+    auto it1 = stopsByID.find(id1);
+    auto it2 = stopsByID.find(id2);
+    if(it1 == stopsByID.end() || it2 == stopsByID.end()) return NO_REGION;
     return NO_REGION;
 }
+
+// ###############################################
+// Phase 2 operations
+// ###############################################
 
 std::vector<RouteID> Datastructures::all_routes()
 {
@@ -252,3 +439,80 @@ void Datastructures::add_walking_connections()
 {
     // Replace this comment and the line below with your implementation
 }
+
+// ###############################################
+// Phase 1 private operations
+// ###############################################
+void Datastructures::getRegions(std::vector<RegionID> &regions, Datastructures::Region* overRegionPtr)
+{
+    if(overRegionPtr == nullptr) return;
+
+    RegionID id = overRegionPtr->id_;
+    regions.push_back(id);
+    if(overRegionPtr->overRegion_ == nullptr)
+    {
+        return;
+    }
+    else
+    {
+        getRegions(regions, overRegionPtr->overRegion_);
+    }
+}
+
+
+void Datastructures::getMinMaxCoords(Coord &min, Coord &max, Datastructures::Region *regionPtr)
+{
+    // initialize if min or max has not valid value and they will be compared
+    if( (min == NO_COORD || max == NO_COORD) && regionPtr->stops_.size() > 0  )
+    {
+        min = regionPtr->stops_.begin()->first;
+        max = regionPtr->stops_.rbegin()->first;
+    }
+    // compare with region own stops
+    if( regionPtr->stops_.size() > 0 )
+    {
+        if(!(regionPtr->minMaxIsValid))
+        {
+            updateRegionMinMax(regionPtr); // theta(n)
+        }
+        Coord* regionMin = &(regionPtr->minMax.first);
+        Coord* regionMax = &(regionPtr->minMax.second);
+        if(min.x > regionMin->x) min.x = regionMin->x;
+        if(min.y > regionMin->y) min.y = regionMin->y;
+        if(max.x < regionMax->x) max.x = regionMax->x;
+        if(max.y < regionMax->y) max.y = regionMax->y;
+    }
+    // call recursively if subregions exist
+    if((int)regionPtr->subRegions_.size() > 0)
+    {
+        for(auto subRegion : regionPtr->subRegions_)
+        {
+            getMinMaxCoords(min, max, subRegion);
+        }
+    }
+    return;
+}
+
+void Datastructures::updateRegionMinMax(Datastructures::Region *regionPtr)
+{
+    if(regionPtr->stops_.size() == 0)
+    {
+        regionPtr->minMaxIsValid = true;
+        return;
+    }
+    Coord rmin = regionPtr->stops_.begin()->first;
+    Coord rmax = regionPtr->stops_.rbegin()->first;
+    for(auto& stop : regionPtr->stops_)
+    {
+        const Coord* coord = &(stop.first);
+        if(rmin.x > coord->x) rmin.x = coord->x;
+        if(rmin.y > coord->y) rmin.y = coord->y;
+        if(rmax.x < coord->x) rmax.x = coord->x;
+        if(rmax.y < coord->y) rmax.y = coord->y;
+    }
+    regionPtr->minMax = std::make_pair(rmin,rmax);
+    regionPtr->minMaxIsValid = true;
+}
+// ###############################################
+// Phase 1 private operations END
+// ###############################################
--- prg2/datastructures.hh
+++ prg2/datastructures.hh
@@ -9,6 +9,11 @@
 #include <utility>
 #include <limits>
 
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <memory> // shared_ptr
+
 // Types for IDs
 using StopID = long int;
 using RegionID = std::string;
@@ -38,13 +43,17 @@ struct Coord
 inline bool operator==(Coord c1, Coord c2) { return c1.x == c2.x && c1.y == c2.y; }
 inline bool operator!=(Coord c1, Coord c2) { return !(c1==c2); } // Not strictly necessary
 
-// Example: Defining < for Coord so that it can be used
-// as key for std::map/set
-inline bool operator<(Coord c1, Coord c2)
+inline bool operator<(Coord c1, Coord c2) // return c1 < c2 (distance from origin)
 {
-    if (c1.y < c2.y) { return true; }
-    else if (c2.y < c1.y) { return false; }
-    else { return c1.x < c2.x; }
+    double d1 = c1.x * c1.x + c1.y * c1.y;
+    double d2 = c2.x * c2.x + c2.y * c2.y;
+
+    if(d1 < d2) return true;
+    else if(d1 > d2) return false;
+    else
+    {
+        return c1.y < c2.y; // if same distance return value according y-coordinate
+    }
 }
 
 // Return value for cases where coordinates were not found
@@ -78,84 +87,93 @@ public:
     Datastructures();
     ~Datastructures();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: theta(1)
+    // Short rationale for estimate: size-funktion toiminta on vakioaikainen
     int stop_count();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: theta(n)
+    // Short rationale for estimate: Jokaisen alkion purkajaa kutsutaan
     void clear_all();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: theta(n)
+    // Short rationale for estimate: Käydään läpi koko tietorakenne ja kopioidaan alkiot
     std::vector<StopID> all_stops();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n), keskimäärin theta(1)
+    // Short rationale for estimate: Pysäkin luonti ja lisäys tietorakenteeseen vakioaikaisia. Find on pahimmassa tapauksessa lineaarinen,
+    // mutta se toteutuu vain silloin, kun yritetään lisätä olemassa olevalla id:llä uutta pysäkkiä. Keskimäärin siis vakioaikainen suoritus.
     bool add_stop(StopID id, Name const& name, Coord xy);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n), keskimäärin theta(1)
+    // Short rationale for estimate: find keskimäärin vakioaikainen, mutta pahimmassa tapauksessa lineaarinen
     Name get_stop_name(StopID id);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n), keskimäärin theta(1)
+    // Short rationale for estimate: find keskimäärin vakioaikainen, mutta pahimmassa tapauksessa lineaarinen
     Coord get_stop_coord(StopID id);
 
-    // We recommend you implement the operations below only after implementing the ones above
-
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: theta(n)
+    // Short rationale for estimate: Pysäkit käydään kertaalleen läpi ja kopioidaan.
     std::vector<StopID> stops_alphabetically();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: theta(n)
+    // Short rationale for estimate: Pysäkit käydään kertaalleen läpi ja kopioidaan.
     std::vector<StopID> stops_coord_order();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: theta(1)
+    // Short rationale for estimate: Suoritus vakioaikainen, koska mapista otetaan vain ensimmäinen alkio ja palautetaan se.
     StopID min_coord();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: theta(1)
+    // Short rationale for estimate: Suoritus vakioaikainen, koska mapista otetaan vain viimeinen alkio ja palautetaan se.
     StopID max_coord();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n), keskimäärin theta(log(n))
+    // Short rationale for estimate: equal_range on logaritminen joten vähintään aikaa kuluu log(n) verran. Pahimmassa tapauksessa kopioidaan
+    //                               koko tietorakenteen StopID:t, jolloin aikaa kuluu n:n verran. Oletuksena on, ettei useita saman nimisiä
+    //                               pysäkkejä ole montaa, jolloin kopiointi on vakioaikaista.
     std::vector<StopID> find_stops(Name const& name);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n)
+    // Short rationale for estimate: Aikaa kuluu etsimiseen find-funktioilla maksimissaan n:n verran.
+    //                               Muut toiminnot eivät kuluta lähes yhtään aikaa.
     bool change_stop_name(StopID id, Name const& newname);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n), keskimäärin theta(log(n))
+    // Short rationale for estimate: Aikaa kuluu etsimiseen find-funktioilla maksimissaan n:n verran, yleensä vähemmän.
+    //                               equal_range käyttää log(n):n verran aikaa.
+    //                               Oletetaan, ettei pysäkin koordinaatteja tarvitse muuttaa usein, sen takia kuluu hieman enemmän aikaa,
+    //                               jotta muut haut olisivat nopeampia
     bool change_stop_coord(StopID id, Coord newcoord);
 
-    // We recommend you implement the operations below only after implementing the ones above
-
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n) keskimäärin theta(1)
+    // Short rationale for estimate: find-funktio on keskimäärin vakioaikainen, muuta pahimmillaan lineaarinen.
+    //                               Olion luonti ja lisäys ei vaikuta paljon aikaan.
     bool add_region(RegionID id, Name const& name);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n), keskimäärin theta(1)
+    // Short rationale for estimate: Find-funktio on huonoimmassa tapauksessa lineaarinen, mutta keskimäärin vakioaikainen.
     Name get_region_name(RegionID id);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: theta(n)
+    // Short rationale for estimate: kaikki alkiot käydään läpi ja kopioidaan vectoriin.
     std::vector<RegionID> all_regions();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n)
+    // Short rationale for estimate: kaksi find-funktiota kuluttavat aikaa keskimäärin vakioaikaisesti, mutta voivat olla lineaarisiakin.
+    //                               Ei kuitenkaan huonompia. Muut toiminnot eivät kuluta paljon aikaa.
     bool add_stop_to_region(StopID id, RegionID parentid);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n)
+    // Short rationale for estimate: kaksi find-funktiota kuluttavat aikaa keskimäärin vakioaikaisesti, mutta voivat olla lineaarisiakin.
+    //                               Ei kuitenkaan huonompia. Muut toiminnot eivät kuluta paljon aikaa.
     bool add_subregion_to_region(RegionID id, RegionID parentid);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n), keskimäärin theta(1)
+    // Short rationale for estimate: find on huonoimmassa tapauksesa lineaarinen.
+    //                                Lisäksi alueet käydäään rekursiivisesti läpi, kunnes ne loppuvat,
+    //                                joten alueiden haku on myös lineaarinen pysäkin alueiden määrän verran.
+    //                                Tässä on oletettu, että alueitä ei ole paljon, jolloin haku on vakioaikainen.
     std::vector<RegionID> stop_regions(StopID id);
 
     // Non-compulsory operations
@@ -164,23 +182,32 @@ public:
     // Short rationale for estimate:
     void creation_finished();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n)
+    // Short rationale for estimate: Pysäkin hakuun kuluu maksimissaan lineaarinen määrä aikaa, mutta keskimäärin vakioaikaisesti.
+    //  Ajankäyttö riippuu vahvasti siitä, ovatko alueiden tallennetut min ja max koordinaatit voimassa. Muut metodit eivät päivitä automaattisesti
+    //  alueen reunakoordinaatteja, kun koordinaatit muuttuvat alueen sisäissä pysäkeissä. Parhaimmassa tapauksessa käydään ainoastaan läpi
+    //  alueet lineaarisesti(ilman pysäkkejä). Pahimmassa tapauksessa yhdenkään alueen reunakoordinaatteja ei olla määritetty valmiiksi, jolloin
+    //  jokaisen alueen pysäkit joudutaan käymään läpi.
     std::pair<Coord, Coord> region_bounding_box(RegionID id);
 
     // Estimate of performance:
     // Short rationale for estimate:
     std::vector<StopID> stops_closest_to(StopID id);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(n)
+    // Short rationale for estimate: pysäkin etsinnässä tehdään pahimmassa tapauksessa lineaarnen haku.
+    //  Yleensä kuitenkin lyhyempi aika. Equal_rangen ajankäyttö on log(n) ja se tehdään kahdesti. Lisäksi
+    //  jos poistettavalla pysäkillä on alue, käydään läpi myös lineaarisesti kaikki alueeseen jäljelle jääneet
+    //  pysäkit min ja max koordinaattien määrittämiseksi. Oletetaan kuitenkin, että metodia ei kutsuta useasti.
     bool remove_stop(StopID id);
 
     // Estimate of performance:
     // Short rationale for estimate:
     RegionID stops_common_region(StopID id1, StopID id2);
 
+    // ###############################################
     // Phase 2 operations
+    // ###############################################
 
     // Estimate of performance:
     // Short rationale for estimate:
@@ -237,7 +264,52 @@ public:
     void add_walking_connections(); // Note! This method is completely optional, and not part of any testing
 
 private:
-    // Add stuff needed for your class implementation here
+    struct Stop;
+
+    struct Region{
+        RegionID id_;
+        Name name_;
+        Region* overRegion_ = nullptr;
+        std::vector<Region*> subRegions_;
+        std::multimap<Coord,Stop*> stops_;
+        std::pair<Coord,Coord> minMax;
+        bool minMaxIsValid;
+    };
+
+    struct Stop {
+        StopID id_;
+        Coord coord_;
+        Name name_;
+        Region* region_ = nullptr;
+    };
+
+    std::unordered_map<StopID,std::shared_ptr<Stop>> stopsByID;
+    std::unordered_map<RegionID,Region> regionsByID;
+    std::multimap<Coord, Stop*> stopCoords; // Järjestyksessä koordinaatin mukaan
+    std::multimap<std::string,Stop*> stopsByName; // Järjestyksessä nimen mukaan
+
+    ///
+    /// \brief getRegions Kerää vectoriin alueet, johon tietty pysäkki kuuluu
+    /// \param regions Kaikki alueet, johon pysäkki kuuluu
+    /// \param overRegionPtr osoitin pysäkin omaan alueeseen
+    ///
+    void getRegions(std::vector<RegionID>& regions, Region* overRegionPtr);
+
+    ///
+    /// \brief getMinMaxCoords Vertailee min ja max koordinaatteja alueen pysäkkien koordinaatteihin
+    ///         ja muuttaa min ja max koordinaatteja jos löytyy suurempia/pienempiä arvoja.
+    ///         Kutsutaan region_bounding_box funktioista.
+    /// \param min reference to the region_bounding_box return value
+    /// \param max reference to the region_bounding_box return value
+    /// \param regionPtr
+    ///
+    void getMinMaxCoords(Coord& min, Coord& max, Region* regionPtr);
+
+    ///
+    /// \brief updateRegionMinMax Päivittää annetun alueen min ja max koordinaatit
+    /// \param regionPtr
+    ///
+    void updateRegionMinMax(Region* regionPtr);
 
 };
 
